36c2ee7de7674a83d4664ea37308077a1dc36da0
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2024-12-19 23:40:45 +0100

    Frontend -> Production Change

--- ML.Frontend/app/axiosInstance.tsx
+++ ML.Frontend/app/axiosInstance.tsx
@@ -1,18 +1,26 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'http://45.140.142.168:8120'
+    : 'https://localhost:7196';
+
 const axiosInstance = axios.create({
-  baseURL: 'https://localhost:7196', 
+  baseURL,
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = Cookies.get('auth-token');
-  if (token && config.url && !config.url.includes('/api/Auth/login') && !config.url.includes('/api/Auth/register')) {
-    config.headers.Authorization = `Bearer ${token}`;
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('auth-token');
+    if (token && config.url && !config.url.includes('/api/Auth/login') && !config.url.includes('/api/Auth/register')) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default axiosInstance;
