991ebf9106e3cff5e56ef4b897b06207c298a9fd
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2024-12-25 23:28:47 +0100

    Database -> Query Changed

--- ML.Backend/ML.API/obj/Debug/net8.0/ML.API.AssemblyInfo.cs
+++ ML.Backend/ML.API/obj/Debug/net8.0/ML.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ML.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31822b20de28f9f413e217d08c8ad1d3b0e0dc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586eeefdd7769a4e1919a0647b6c70c57491af21")]
 [assembly: System.Reflection.AssemblyProductAttribute("ML.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ML.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
